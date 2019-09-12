b73d348a0c4b4c185c22fd37fed099db0854c7c8
tom-desktop <mooseinpursuit@gmail.com>
2019-09-12 12:42:22 +1000

    testing

--- test/gaussian_background.py
+++ test/gaussian_background.py
@@ -1,3 +1,13 @@
 import numpy as np
 
-print('hi')
+from glob import glob      # for batch loading files
+
+import os
+import utils
+root = utils.get_project_root()
+
+dir = "test"
+img_dir = "img/img_campus"
+
+if __name__ == "__main__":
+    print(root)
