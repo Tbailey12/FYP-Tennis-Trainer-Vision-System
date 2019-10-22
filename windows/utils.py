422c801329a548ac8b1f7870de097b77f478d137
tom-desktop <mooseinpursuit@gmail.com>
2019-10-22 13:52:06 +1100

    rearranged some folders

--- windows/utils.py
+++ windows/utils.py
@@ -1,5 +1,10 @@
 from pathlib import Path
+import numpy as np
 
 def get_project_root():
     ''' Returns the path to the root project dir'''
     return Path(__file__).parent.parent
+
+def rgb_to_gray(rgb):
+    return np.dot(rgb[...,:3], [0.2989, 0.5870, 0.1140])
+
