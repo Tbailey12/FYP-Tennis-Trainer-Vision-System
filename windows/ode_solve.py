c22e890fb2c44b3701189a04d3fc8b81727387ed
tom-desktop <mooseinpursuit@gmail.com>
2019-11-01 20:46:30 +1100

    wind added

--- windows/ode_solve.py
+++ windows/ode_solve.py
@@ -16,7 +16,7 @@ d = 1.21;  # air density (kg/m^3)
 m = 58E-3;  # mass in kg
 # cd = 0.55;  # for a new ball (change later to formula based on CL)
 # cl = 0;  # change later to formula
-g = 9.81;  # gravitational acceleration
+g = 9.807;  # gravitational acceleration
 
 
 def calc_cl(v, vspin, spin_dir):
@@ -33,9 +33,14 @@ def calc_cd(v, vspin):
         return 0.55
 
 
-def solve_numeric(v0, elev, azimuth, spin, spin_dir, x0, y0, z0, start, end, num_points):
+def solve_numeric(v0, elev, azimuth, spin, spin_dir, x0, y0, z0, start, end, num_points, wind_v, wind_azimuth):
     elev_r = elev * (np.pi / 180);  # launch elevation in radians
     azimuth_r = azimuth * (np.pi / 180);  # launch azimuth in radians
+    wind_azimuth_r = wind_azimuth * (np.pi / 180);  # wind azimuth in radians
+
+    cd_w = 0.55  # drag coefficient for wind
+    vx_w = wind_v * np.sin(wind_azimuth_r)
+    vy_w = wind_v * np.cos(wind_azimuth_r)
 
     vx0 = v0 * np.cos(elev_r) * np.sin(azimuth_r)  # initial velocity
     vy0 = v0 * np.cos(elev_r) * np.cos(azimuth_r)  # initial y velocity
@@ -49,8 +54,8 @@ def solve_numeric(v0, elev, azimuth, spin, spin_dir, x0, y0, z0, start, end, num
     t = np.linspace(start, end, num=num_points)
 
     def model(x, t):
-        vx = x[0] / m
-        vy = x[2] / m
+        vx = x[0] / m - vx_w
+        vy = x[2] / m - vy_w
         vz = x[4] / m
 
         v = np.sqrt(vx ** 2 + vy ** 2 + vz ** 2)
@@ -60,9 +65,9 @@ def solve_numeric(v0, elev, azimuth, spin, spin_dir, x0, y0, z0, start, end, num
         cl = calc_cl(v, vspin, spin_dir)  # coefficient of lift
 
         dPxdt = (-A * d * v / 2) * (cl * vz * np.sin(azimuth_r) + cd * vx)
-        dxdt = vx
+        dxdt = vx + vx_w
         dPydt = (-A * d * v / 2) * (cl * vz * np.cos(azimuth_r) + cd * vy)
-        dydt = vy
+        dydt = vy + vy_w
         dPzdt = (A * d * v / 2) * (cl * v_p - cd * vz) - m * g
         dzdt = vz
 
@@ -75,17 +80,16 @@ def solve_numeric(v0, elev, azimuth, spin, spin_dir, x0, y0, z0, start, end, num
 
 
 if __name__ == "__main__":
-    z1 = solve_numeric(30, 8.1, 0, 0, TOPSPIN, 0, 0, 1, 0, 5, 500)
-    z2 = solve_numeric(30, 8.1, 0, 2500, TOPSPIN, 0, 0, 1, 0, 5, 500)
-    z3 = solve_numeric(30, 8.1, 0, 2500, BACKSPIN, 0, 0, 1, 0, 5, 500)
+    z1 = solve_numeric(30, 8.1, 0, 0, BACKSPIN, 0, 0, 1, 0, 5, 1000, 0, 90)
+    z2 = solve_numeric(30, 11.9, 0, 20*60, TOPSPIN, 0, 0, 1, 0, 5, 1000, 0, 90)
 
-    z = [z1, z2, z3]
+    z = [z1,z2]
 
     fig = plt.figure()
     ax = plt.axes(projection='3d')
     # plot results
     for z_arr in z:
-        z_arr = z_arr[z_arr[:,5]>0]
+        z_arr = z_arr[z_arr[:, 5] > 0]
         ax.plot3D(z_arr[:, 1], z_arr[:, 3], z_arr[:, 5])
     ax.set_xlabel('x')
     ax.set_ylabel('y')
@@ -93,5 +97,6 @@ if __name__ == "__main__":
     ax.set_xlim(-10.97 / 2, 10.97 / 2)
     ax.set_ylim(0, 11.89 * 2)
     ax.set_zlim(0, 5)
-    ax.legend(['Spin = 0rpm','Topspin = 2500rpm','Backspin = 2500rpm'])
+    ax.legend(['Spin = 0rpm', 'Topspin = 2500rpm', 'Backspin = 2500rpm'])
+    plt.title('Backspin/Topspin')
     plt.show()
