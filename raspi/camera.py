5ffb47585470c20dfa34d35961b391f47559d582
Tom-Pi4 <tom.bailey@outlook.com.au>
2020-01-09 09:30:52 +1100

    after RGB analysis

--- raspi/camera.py
+++ raspi/camera.py
@@ -2,6 +2,7 @@ import io
 import time
 import threading
 import picamera
+import picamera.array
 import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
@@ -19,39 +20,44 @@ def print_l(print_lock, message):
         print_lock.release()
 
 def ImageProcessor(unprocessed_frames, print_lock, process_run):
-    stream = io.BytesIO()
+    # stream = io.BytesIO()
     # self.event = multiprocessing.Event()
 
     while process_run:
         try:
             # attempts to get a new frame, raises exception if none available
-            
+            pass
             # print(unprocessed_frames.qsize())
 
-            frame_num, frame = unprocessed_frames.get_nowait()
-            stream.write(frame)
-            stream.seek(0)
-            # Read the image and do some processing on it
+            # frame_num, frame = unprocessed_frames.get_nowait()
+            # print_lock.acquire()
+            # print(unprocessed_frames.qsize())
+            # print_lock.release()
+            # stream.write(frame)
+            # stream.seek(0)
+            # # Read the image and do some processing on it
 
-            im = Image.open(stream) # read image from stream
-            img = np.array(im) # convert image to numpy array
+            # im = Image.open(stream) # read image from stream
+            # img = np.array(im) # convert image to numpy array
 
-            # print_l(print_lock,img)
-            print_lock.acquire()
-            cv2.imwrite(f'{frame_num:03d}.png',img)
-            # cv2.imwrite(f'{frame_num:03.3f}.png',img)
-            print_lock.release()
-            # Reset the stream
-            stream.seek(0)
-            stream.truncate()
+            # # print_l(print_lock,img)
+            # print_lock.acquire()
+            # cv2.imwrite(f'{frame_num:03d}.png',frame)
+            # # cv2.imwrite(f'{frame_num:03.3f}.png',img)
+            # print_lock.release()
+            # # Reset the stream
+            # stream.seek(0)
+            # stream.truncate()
 
         except queue.Empty:
             if process_run.value is False:
                 break
             continue
     return
-class ProcessOutput(object):
-    def __init__(self):
+
+class MyAnalysisClass(picamera.array.PiRGBAnalysis):
+    def __init__(self, camera):
+        super(MyAnalysisClass, self).__init__(camera)
         self.number_of_processors = 4
         self.unprocessed_frames = multiprocessing.Queue()
         self.processes = []
@@ -66,49 +72,89 @@ class ProcessOutput(object):
             self.processes.append(processor)
             processor.start()
 
-    def write(self, buf):
+    def analyze(self, array):
+        # print(time.time()-self.start)
+        # self.start = time.time()
         if self.done is False:
-            if buf.startswith(b'\xff\xd8'): # start of a new frame
-                # print(f'frame: {self.frame_num}')
-                self.unprocessed_frames.put((self.frame_num, buf)) # add the new frame to the buffer
-                # self.unprocessed_frames.put((time.time(), buf)) # add the new frame to the buffer
-                self.frame_num += 1
+            # print(self.frame_num)
+            # self.unprocessed_frames.put((self.frame_num, array)) # add the new frame to the buffer
+            # self.print_lock.acquire()
+            # self.unprocessed_frames.put(self.frame_num) # add the new frame to the buffer
+            # self.print_lock.release()
+            # self.unprocessed_frames.put((time.time(), buf)) # add the new frame to the buffer
+            self.frame_num += 1
+            # print (time.time()-start)
+            print(self.frame_num)
 
     def flush(self):
         print('closing camera')
         # print('flushing')
         # When told to flush (this indicates end of recording), shut
         # down in an orderly fashion.
-
         for p in self.processes:
             p.terminate()
             p.join()
 
-with picamera.PiCamera(resolution='1280x720',framerate=90) as camera:
-    #camera.start_preview()
-    camera.vflip=True
-    camera.annotate_frame_num = True
-    camera.annotate_text_size = 160
-    camera.annotate_foreground = picamera.Color('green')
-    time.sleep(2)
-    output = ProcessOutput()
-    camera.start_recording(output, format='mpjeg')
+# class ProcessOutput(object):
+#     def __init__(self):
+#         self.number_of_processors = 4
+#         self.unprocessed_frames = multiprocessing.Queue()
+#         self.processes = []
+#         self.print_lock = multiprocessing.Lock()
+#         self.frame_num = 0
+#         self.start = time.time()
+#         self.done = False
+#         self.process_run = multiprocessing.Value(ctypes.c_bool,True)
 
-    try:   
-        camera.wait_recording(1)
-    finally:
-        output.process_run.value = False    # ensures all processes stop on next run
-        output.done = True  # stop writing frames to queue
+#         for i in range(self.number_of_processors):
+#             processor = multiprocessing.Process(target=ImageProcessor, args=(self.unprocessed_frames, self.print_lock, self.process_run))
+#             self.processes.append(processor)
+#             processor.start()
 
-    while(True):
-        dead_count = 0
-        for process in output.processes:
-            if process.is_alive() is False:
-                dead_count+=1
-        if dead_count == output.number_of_processors:
-            break
+#     def write(self, buf):
+#         if self.done is False:
+#             if buf.startswith(b'\xff\xd8'): # start of a new frame
+#                 # print(f'frame: {self.frame_num}')
+#                 self.unprocessed_frames.put((self.frame_num, buf)) # add the new frame to the buffer
+#                 # self.unprocessed_frames.put((time.time(), buf)) # add the new frame to the buffer
+#                 self.frame_num += 1
 
-    camera.stop_recording()
+#     def flush(self):
+#         print('closing camera')
+#         # print('flushing')
+#         # When told to flush (this indicates end of recording), shut
+#         # down in an orderly fashion.
+
+#         for p in self.processes:
+#             p.terminate()
+#             p.join()
+
+with picamera.PiCamera(resolution='1280x720',framerate=50) as camera:
+    with picamera.array.PiRGBAnalysis(camera) as output:
+        #camera.start_preview()
+        camera.vflip=True
+        camera.annotate_frame_num = True
+        camera.annotate_text_size = 160
+        camera.annotate_foreground = picamera.Color('green')
+        time.sleep(2)
+        output = MyAnalysisClass(camera)
+        camera.start_recording(output, format='bgr')
+
+        try:   
+            camera.wait_recording(1)
+        finally:
+            output.process_run.value = False    # ensures all processes stop on next run
+            output.done = True  # stop writing frames to queue
+
+        while(True):
+            dead_count = 0
+            for process in output.processes:
+                if process.is_alive() is False:
+                    dead_count+=1
+            if dead_count == output.number_of_processors:
+                break
+
+        camera.stop_recording()
 
     # output = ProcessOutput()
     # camera.start_recording(output, format='mjpeg')
