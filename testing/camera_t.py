1b2f748cecbaba96b6c07f20ab463890db2fba12
tom-desktop <tom.bailey@outlook.com.au>
2020-03-22 16:07:55 +1100

    added data analysis true/false pos to camera_t.py

--- testing/camera_t.py
+++ testing/camera_t.py
@@ -4,6 +4,7 @@ import time
 import multiprocessing as mp
 import queue
 import numpy as np
+from operator import itemgetter
 
 TEST_F = 'stereo_tests'
 IMG_F = 'img'
@@ -15,8 +16,9 @@ OUT_F = 'out'
 IMG_MEAN_F = 'img_mean.npy'
 IMG_STD_F = 'img_std.npy'
 
-ROOT_P = os.getcwd()
-TEST_P = ROOT_P + '//' + TEST_F
+ROOT_P = 'D:\\documents\\local uni\\FYP\\code'
+
+TEST_P = ROOT_P + '\\' + 'img' + '\\' + TEST_F
 
 RESOLUTION = (640,480)
 w,h = RESOLUTION
@@ -28,18 +30,37 @@ SIZE = 2
 X = 3
 Y = 4
 
-kernel = np.array([	[0,1,0],
-					[1,1,1],
-					[0,1,0]], dtype=np.uint8)
+# kernel = np.array([	[0,1,0],
+# 					[1,1,1],
+# 					[0,1,0]], dtype=np.uint8)
 
-kernel1 = np.array([[0,0,1,0,0],
+# kernel1 = np.array([[0,0,1,0,0],
+# 					[0,1,1,1,0],
+# 					[1,1,1,1,1],
+# 					[0,1,1,1,0],
+# 					[0,0,1,0,0]], dtype=np.uint8)
+
+kernel = np.ones((2,2), dtype=np.uint8)
+
+kernel1 = np.ones((3,3), dtype=np.uint8)
+
+kernel2 = np.ones((4,4), dtype=np.uint8)
+
+kernel3 = np.ones((5,5), dtype=np.uint8)
+
+kernelt3 = np.array([ 	[0,1,0],
+                    	[1,1,1],
+                    	[0,1,0]], dtype=np.uint8)
+
+kernelc5 = np.array([[0,0,1,0,0],
 					[0,1,1,1,0],
 					[1,1,1,1,1],
 					[0,1,1,1,0],
 					[0,0,1,0,0]], dtype=np.uint8)
 
+
 def process_img(img_f, img_queue, active_test_dir, out_q):
-	os.chdir(TEST_P + '//' + img_f + '//' + DATA_F + '//' + active_test_dir)
+	os.chdir(TEST_P + '\\' + img_f + '\\' + DATA_F + '\\' + active_test_dir)
 	
 	A =  np.zeros([h,w],dtype=np.uint8)
 	B = np.zeros([h,w],dtype=np.uint8)
@@ -65,12 +86,16 @@ def process_img(img_f, img_queue, active_test_dir, out_q):
 	img_mean = np.load(IMG_MEAN_F)
 	img_std = np.load(IMG_STD_F)
 
+	total_time = 0
+	count = 0
 	while True:
 		try:
 			frame, y_data = img_queue.get_nowait()
 			if y_data is None:
 				img_queue.task_done()
 				return
+
+			start = time.time_ns()
 
 			## -- Process Image -- ##
 			B_old = np.copy(B)
@@ -88,17 +113,24 @@ def process_img(img_f, img_queue, active_test_dir, out_q):
 
 
 			# C = B
-			C = 255*B.astype(np.uint8)
+			C = 255*C.astype(np.uint8)
+			C = cv2.filter2D(C, ddepth = -1, kernel=(1/16)*kernel2)
+			C[C<174] = 0
+			C[C>=174] = 255
 
 			# C = cv2.morphologyEx(C, cv2.MORPH_CLOSE, kernel, iterations=1)
-			C = cv2.dilate(C, kernel, iterations=1)
-			C = cv2.erode(C, kernel, iterations=2)
+			# C = cv2.dilate(C, kernel, iterations=1)
+			# C = cv2.erode(C, kernel, iterations=2)
 
+			# C = cv2.morphologyEx(C, cv2.MORPH_CLOSE, kernelt3, iterations=1)
+			# C = cv2.erode(C, kernelc5, iterations=1)
 			# for i in ec_order:
 			# 	if i == 'e':
 			# 		C = cv2.erode(C, k1, iterations=1)
 			# 	elif i == 'c':
 			# 		C = cv2.morphologyEx(C, cv2.MORPH_CLOSE, k2, iterations=1)
+
+			total_time = total_time + (time.time_ns()-start)
 
 			# calculate new std deviation and mean
 			# y_data = np.bitwise_and(y_data, np.invert(C))
@@ -119,15 +151,15 @@ def process_img(img_f, img_queue, active_test_dir, out_q):
 			## -- object detection -- ##
 			n_features_cv, labels_cv, stats_cv, centroids_cv = cv2.connectedComponentsWithStats(C, connectivity=4)
 
-			label_mask_cv = np.logical_and(stats_cv[:,cv2.CC_STAT_AREA]>5, stats_cv[:,cv2.CC_STAT_AREA]<10000)
+			label_mask_cv = np.logical_and(stats_cv[:,cv2.CC_STAT_AREA]>4, stats_cv[:,cv2.CC_STAT_AREA]<10000)
 			ball_candidates = np.concatenate((stats_cv[label_mask_cv,2:],centroids_cv[label_mask_cv]), axis=1)
 
 			# sort ball candidates by size and keep the top 100
-			ball_candidates = ball_candidates[ball_candidates[:,SIZE].argsort()[::-1][:N_OBJECTS]]
+			# ball_candidates = ball_candidates[ball_candidates[:,SIZE].argsort()[::-1][:N_OBJECTS]]
 
-			out_q.put((frame, ball_candidates))
+			out_q.put([frame, ball_candidates])
 
-			os.chdir(TEST_P + '//' + img_f + '//' + IMG_F + '//' + active_test_dir)
+			os.chdir(TEST_P + '\\' + img_f + '\\' + IMG_F + '\\' + active_test_dir)
 			img = cv2.imread(f"{frame}.png")
 
 			C = cv2.cvtColor(C, cv2.COLOR_GRAY2RGB)
@@ -137,16 +169,23 @@ def process_img(img_f, img_queue, active_test_dir, out_q):
 				cv2.drawMarker(C,(ball[X],ball[Y]),(0, 0, 255),cv2.MARKER_CROSS,thickness=2,markerSize=10)
 				cv2.drawMarker(img,(ball[X],ball[Y]),(0, 0, 255),cv2.MARKER_CROSS,thickness=2,markerSize=10)
 
-			os.chdir(TEST_P + '//' + img_f + '//' + OUT_F + '//' + active_test_dir)
+			os.chdir(TEST_P + '\\' + img_f + '\\' + OUT_F + '\\' + active_test_dir)
 			cv2.imwrite(f"C{frame}.png", C)
 			cv2.imwrite(f"{frame}.png", img)
 
+			count += 1
 			img_queue.task_done()
+
+			if total_time > 0:
+				print(f"{mp.current_process()}: {(total_time/1E9)/count}")
 		except queue.Empty:
+			if total_time > 0:
+				print(total_time/1E9)
+				total_time = 0
 			pass
 
 def read_img(cam_f, queue_list, active_test_dir):
-	os.chdir(TEST_P + '//' + cam_f + '//' + IMG_F + '//' + active_test_dir)
+	os.chdir(TEST_P + '\\' + cam_f + '\\' + IMG_F + '\\' + active_test_dir)
 	img_list = os.listdir()
 	
 	cur_queue = 0
@@ -168,9 +207,9 @@ if __name__ == "__main__":
 	ball_candidates_out = []
 
 	# get list of test data directories
-	os.chdir(TEST_P + '//' + LEFT_F + '//' + DATA_F)
+	os.chdir(TEST_P + '\\' + LEFT_F + '\\' + DATA_F)
 	test_directories = os.listdir()
-	os.chdir(TEST_P + '//' + LEFT_F + '//' + OUT_F)
+	os.chdir(TEST_P + '\\' + LEFT_F + '\\' + OUT_F)
 	for directory in test_directories:
 		try:
 			os.mkdir(directory)
@@ -179,9 +218,8 @@ if __name__ == "__main__":
 
 	# directory = test_directories[4]
 	# print(directory)
-	for directory in test_directories[6:]:
+	for directory in test_directories[4:5]:
 		active_test_dir = str(directory)
-		os.chdir(ROOT_P)
 		process_list = []
 		queue_list = []
 		out_q = mp.JoinableQueue()
@@ -208,28 +246,60 @@ if __name__ == "__main__":
 
 			except queue.Empty:
 				break
+		
+		# sort all the ball candidates
+		ball_candidates_out.sort(key=itemgetter(0))
 
-		# os.chdir(TEST_P + '//' + LEFT_F + '//' + DATA_F)
+		# os.chdir(TEST_P + '\\' + LEFT_F + '\\' + DATA_F)
 		# np.save('right_ball_candidates.npy', ball_candidates_out)
 
+		os.chdir(TEST_P + '\\' + LEFT_F + '\\' + DATA_F + '\\' + active_test_dir)
+		points = np.load("points_full.npy", allow_pickle=True)
 		# make video from image dif
 		img_list = []
 		C_list = []
 
-		os.chdir(TEST_P + '//' + LEFT_F + '//' + OUT_F + '//' + active_test_dir)
+		os.chdir(TEST_P + '\\' + LEFT_F + '\\' + OUT_F + '\\' + active_test_dir)
 		im_names = os.listdir()
+		print(len(im_names))
+		pos = 0
+		dist = 5
 		for im in im_names:
 			if 'C' in im:
-				C_list.append(cv2.imread(im))
+				img = cv2.imread(im)
+				C_list.append(img)
 			else:
-				img_list.append(cv2.imread(im))
+				img = cv2.imread(im)
+				img_list.append(img)
+				num_c = len(ball_candidates_out[pos][1])
+				num_true = 0
+				num_balls = 0
+				num_false = 0
+				if points[pos,0,0] is not None:
+					num_balls = 1
+				for cand in ball_candidates_out[pos][1]:
+					# print(points[pos,0,0])
+					if points[pos,0,0] is not None:
+						if np.sqrt((cand[X]-points[pos,0,0])**2+(cand[Y]-points[pos,0,1])**2) <= dist:
+							num_true += 1
+							num_false = num_c-num_true
+					else:
+						num_false = num_c
+				# if pos%4 == 0:
+				print(f"{num_balls}	{num_true}	{num_false}")
+					# print(f"{len(ball_candidates_out[pos][1])}")
+					# if pos>=136:
+					# 	cv2.imshow(f"{pos}", img)
+					# 	cv2.waitKey(0)
+					# 	cv2.destroyAllWindows()
+				pos+=1
 
-		# os.chdir(TEST_P + '//' + RIGHT_F + '//' + OUT_F + '//' + active_test_dir)
+		# os.chdir(TEST_P + '\\' + RIGHT_F + '\\' + OUT_F + '\\' + active_test_dir)
 		# im_names = os.listdir()
 		# for im in im_names:
 		# 	C_list.append(cv2.imread(im))
 
-		os.chdir(TEST_P + '//' + LEFT_F + '//' + OUT_F)
+		os.chdir(TEST_P + '\\' + LEFT_F + '\\' + OUT_F)
 		
 		output = f"{active_test_dir}.mp4"
 		fourcc = cv2.VideoWriter_fourcc(*'mp4v')
