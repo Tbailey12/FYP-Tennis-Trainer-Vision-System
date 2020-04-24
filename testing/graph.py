c36052071c375d692c523bd99c6ceb8739c87826
tom-desktop <tom.bailey@outlook.com.au>
2020-04-24 13:30:07 +1000

    merged all graph code into functions

--- testing/graph.py
+++ testing/graph.py
@@ -11,38 +11,27 @@ class Tracklet(object):
 		self.con_est = 0
 		self.is_valid = True
 
-tracklets = []
+def create_graph(list):
+	graph = {}
+	for i in range(len(list)):
+		graph[i] = []
 
-for i in range(10):
-	tracklet = Tracklet(i, score=i+1)
-	tracklets.append(tracklet)
+	return graph
 
-graph = {	0: [2,3],
-			1: [4],
-			2: [4],
-			3: [],
-			4: [6,7,8],
-			5: [],
-			6: [],
-			7: [9],
-			8: [],
-			9: []}
+def get_start_end_nodes(graph):
+	start_nodes = [x for x in range(len(graph))]
+	end_nodes = []
 
-# -- get all graph nodes that have no connections
-start_nodes = [x for x in range(len(tracklets))]
-end_nodes = []
+	for node, conn in graph.items():
+		if conn == []:
+			end_nodes.append(node)
+		for c in conn:
+			if c in start_nodes:
+				start_nodes.remove(c)
 
-for node, conn in graph.items():
-	if conn == []:
-		end_nodes.append(node)
-	for c in conn:
-		if c in start_nodes:
-			start_nodes.remove(c)
+	return start_nodes, end_nodes
 
-longest_path = {}
-
-def get_longest_path(graph, start, end=None, path=[], score=0):
-
+def get_longest_paths(tracklets, graph, start, path=[], score=0):
 	if start not in path:
 		score-= tracklets[start].score
 		path.append(start)
@@ -52,15 +41,38 @@ def get_longest_path(graph, start, end=None, path=[], score=0):
 
 	for node in graph[start]:
 		if node not in path:
-			get_longest_path(graph, node, end, path, score)
+			get_longest_paths(tracklets, graph, node, path, score)
 
 	del path[-1]
 
-for node_s in start_nodes:
-	for node, conn in graph.items():
-		longest_path[node] = {'score':0, 'path':[]}
+if __name__ == "__main__":
+	tracklets = []
 
-	get_longest_path(graph, node_s)
-	
-	for node_e in end_nodes:
-		print(longest_path[node_e])
+	for i in range(10):
+		tracklet = Tracklet(i, score=i+1)
+		tracklets.append(tracklet)
+
+	# graph = create_graph(tracklets)
+
+	graph = {	0: [2,3],
+				1: [4],
+				2: [4],
+				3: [],
+				4: [6,7,8],
+				5: [],
+				6: [],
+				7: [9],
+				8: [],
+				9: []}
+
+	start_nodes, end_nodes = get_start_end_nodes(graph)
+	longest_path = {}
+
+	for node_s in start_nodes:
+		for node, conn in graph.items():
+			longest_path[node] = {'score':0, 'path':[]}
+
+		get_longest_paths(tracklets, graph, node_s)
+		
+		for node_e in end_nodes:
+			print(longest_path[node_e])
